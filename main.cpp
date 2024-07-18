a835d01f6780663f75cb93d12eb26794b7501c22
Mateus de Oliveira <163180311+Mateuseng04@users.noreply.github.com>
2024-07-18 17:51:37 +0000

    .

--- main.cpp
+++ main.cpp
@@ -7,24 +7,28 @@
 
 int main(int argc, char* argv[]) 
 {
-  std::string path_script(argc > 1 ? argv[1] : "scripts/entrada.txt");
-  std::string path_output(argc > 2 ? argv[2] : "output.off");
+    std::string path_script(argc > 1 ? argv[1] : "scripts/entrada.txt");
+    std::string path_output(argc > 2 ? argv[2] : "output.off");
 
-  Interpreter Inter;
-  std::vector<FiguraGeometrica*> figs = Inter.compile(path_script);
-  Sculptor *model = new model (Inter.getDimx(), Inter.getDimy(), Inter.getDimz());
-  std::cout << "Criando modelo 3D" << std::endl;
-  for (i = 1; i < (int) figs.size; i++) 
-  {
-    figs[i]->draw(*model);
-  }
+    Interpretador Inter;
+    std::vector<FiguraGeometrica*> figs = Inter.compile(path_script);
+    Sculptor *model = new Sculptor(Inter.getDimx(), Inter.getDimy(), Inter.getDimz());
 
-  model -> writeOFF(path_output);
+    std::cout << "Criando modelo 3D" << std::endl;
 
-  for (i = 0; i < (int) figs.size; i++) {
-    delete figs[i];
-  }
-  delete model;
+    for (int i = 0; i < (int)figs.size(); i++) 
+    {
+        figs[i]->draw(*model);
+    }
 
-  return 0;
+    model->writeOFF(path_output);
+
+    for (int i = 0; i < (int)figs.size(); i++) 
+    {
+        delete figs[i];
+    }
+
+    delete model;
+
+    return 0;
 }
