75f5aad8bb270d9904576d1cde119d1d9b7747f2
Mateus de Oliveira <163180311+Mateuseng04@users.noreply.github.com>
2024-07-18 22:41:44 -0300

    Update CutBox.h

--- include/CutBox.h
+++ include/CutBox.h
@@ -3,17 +3,17 @@
 #include "FiguraGeometrica.h"
 
 
-class CutBox::FiguraGeometrica {
+class CutBox : public FiguraGeometrica {
 private:
   int x0, x1, y0, y1, z0, z1;
 
 public:
 
-  CutBox(int x0, int x1, int y0, int y1);
+  CutBox(int x0, int x1, int y0, int y1, int z0, int z1);
   
-  ~CutBox(){}
+  ~CutBox();
 
   void draw(Sculptor &t);
 };
 
-#endif CUTBOX_H
+#endif
