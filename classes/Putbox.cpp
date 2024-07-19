f2b98d29a3c158ad675e5e8a90d052919f09479a
Mateus de Oliveira <163180311+Mateuseng04@users.noreply.github.com>
2024-07-18 22:49:21 -0300

    Update Putbox.cpp

--- classes/Putbox.cpp
+++ classes/Putbox.cpp
@@ -1,20 +1,20 @@
 #include "PutBox.h"
-#include <iostream.h>
+#include <iostream>
 
-Putbox::Putbox (int x0, int y0, int z0, int x1, int y1, int z1, float r, float g, float b, float a) {
-  this x0 -> x0;
-  this y0 -> y0;
-  this z0 -> z0;
-  this x1 -> x1;
-  this y1 -> y1;
-  this z1 -> z1;
-  this r -> r;
-  this g -> g;
-  this b -> b;
-  this a -> a;
+PutBox::PutBox(int x0, int y0, int z0, int x1, int y1, int z1, float r, float g, float b, float a) {
+  this->x0 = x0;
+  this->y0 = y0;
+  this->z0 = z0;
+  this->x1 = x1;
+  this->y1 = y1;
+  this->z1 = z1;
+  this->r = r;
+  this->g = g;
+  this->b = b;
+  this->a = a;
 }
-void Putbox::draw (Sculptor &s) {
+void PutBox::draw (Sculptor &s) {
   std::cout << "desenhando um cubo em ( " << x0 << "," << y0 << "," << z0 << std::endl;
-  s.setColor(r, g, b, a);
+  s.SetColor(r, g, b, a);
   s.PutBox(x0, y0, z0, x1, y1, z1);
 }
