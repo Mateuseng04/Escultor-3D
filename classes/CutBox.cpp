6d5ca2bb6c12b30067ba249978280e28df0a6e03
Mateus de Oliveira <163180311+Mateuseng04@users.noreply.github.com>
2024-07-18 22:41:03 -0300

    Update CutBox.cpp

--- classes/CutBox.cpp
+++ classes/CutBox.cpp
@@ -1,14 +1,16 @@
 #include "CutBox.h"
-#include <iostream.h>
+#include <iostream>
 
-Cutbox::Cutbox (int x0, int y0, int z0, int x1, int y1) {
-  this x0 -> x0;
-  this y0 -> y0;
-  this z0 -> z0;
-  this x1 -> x1;
-  this y1 -> y1;
-  this z1 -> z1;
+CutBox::CutBox(int x0, int y0, int z0, int x1, int y1, int z1)
+{
+  this->x0 = x0;
+  this->y0 = y0;
+  this->z0 = z0;
+  this->x1 = x1;
+  this->y1 = y1;
+  this->z1 = z1;
 }
-Cutbox::draw(Sulptor &s){
+void CutBox::draw(Sculptor &s)
+{
   std::cout << "cortar cubo em " << x0 << "," << y0 << "," << z0 << std::endl;
 }
