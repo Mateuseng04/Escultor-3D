c6b53bfa19479833484ba4c4d22f55ee579a4019
Mateus de Oliveira <163180311+Mateuseng04@users.noreply.github.com>
2024-07-18 22:50:12 -0300

    Update PutDisc.cpp

--- classes/PutDisc.cpp
+++ classes/PutDisc.cpp
@@ -1,22 +1,23 @@
 #include "PutDisc.h"
-#include <iostream.h>
+#include <iostream>
 
-PutDisc::PutDisc(int xcenter, int ycenter, int zcenter, int radius, int height, int axis, float g, float b, float alpha){
-
-  this xcenter = xcenter;
-  this ycenter = ycenter;
-  this zcenter = zcenter;
-  this radius = radius;
-  this height = height;
-  this r = r;
-  this g = g;
-  this b = b;
-  this alpha = alpha;
+PutDisc::PutDisc(int xcenter, int ycenter, int zcenter, int radius, int height, int axis, float r, float g, float b, float a)
+{
+  this->xcenter = xcenter;
+  this->ycenter = ycenter;
+  this->zcenter = zcenter;
+  this->radius = radius;
+  this->height = height;
+  this->r = r;
+  this->g = g;
+  this->b = b;
+  this->a = a;
+  
   
 }
 
 void PutDisc::draw(Sculptor &s){
-  std::cout << "Desenhando disco em ( " << xcenter << "," << ycenter << "," << zcenter ")\n"::std::endl;
-  s.setcolor(r, g, b, a);
+  std::cout << "Desenhando disco em ( " << xcenter << "," << ycenter << "," << zcenter << ::std::endl;
+  s.SetColor(r, g, b, a);
   s.PutDisc(xcenter, ycenter, zcenter, radius, height, axis);
 }
