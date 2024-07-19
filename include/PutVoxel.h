7921b663c26a0b8106003d0c47f9cfa7f7c94ad8
Mateus de Oliveira <163180311+Mateuseng04@users.noreply.github.com>
2024-07-18 22:54:30 -0300

    Update PutVoxel.h

--- include/PutVoxel.h
+++ include/PutVoxel.h
@@ -1,16 +1,18 @@
-#ifndef  PUTVOXEL_H
-#define  PUTVOXEL_H
+#ifndef  _PUTVOXEL_H_
+#define  _PUTVOXEL_H_
+#include "FiguraGeometrica.h"
 
-class PutVoxel::FiguraGeometrica {
-private:
-  int nx, ny, nz;
+class PutVoxel : public FiguraGeometrica {
+// private:
+ // int nx, ny, nz;
+int x, y, z;
 
 public:
   PutVoxel(int x, int y, int z, float r, float g, float b, float a);
   ~PutVoxel(){}
 
 
-  void draw(Sculptor &t);
+void draw(Sculptor &t);
 };
 
 #endif  // PUTVOXEL_H
