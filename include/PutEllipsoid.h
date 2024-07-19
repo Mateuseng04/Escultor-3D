41ace99e5dd0f01dfd169d24b8384e405c74ec45
Mateus de Oliveira <163180311+Mateuseng04@users.noreply.github.com>
2024-07-18 22:52:29 -0300

    Update PutEllipsoid.h

--- include/PutEllipsoid.h
+++ include/PutEllipsoid.h
@@ -4,17 +4,17 @@
 #include "Sculptor.h"
 
 
-class PutEllipsoid::FiguraGeometrica{
+class PutEllipsoid : public FiguraGeometrica{
 
 private:
   int xcenter, ycenter, zcenter, rx, ry, rz;
   
 public:
-  PutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int r);
+  PutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz, float r, float g, float b, float a);
   ~PutEllipsoid(){}
 
   void draw(Sculptor &t);
 
 };
 
-#endif PUTELLIPSOID_H
+#endif 
